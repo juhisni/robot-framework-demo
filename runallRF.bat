b0e26c67011d8ee93a23cc9614cc3dde4fc83b5b
Juhis <juhis.ni@gmail.com>
2019-02-16 19:31:11 +0200

    fix

--- runallRF.bat
+++ runallRF.bat
@@ -7,6 +7,5 @@ SET TAG=%3
 IF %ENVIRONMENT%==qa goto :execute_in_qa
 
 :execute_in_qa
-call slackviesti.bat
 echo "Starts with arguments ENV=%ENVIRONMENT% BROWSER=%BROWSER% TAG=%TAG% in QA environment."
 robot --variable ENVIRONMENT:%ENVIRONMENT% --variable BROWSER:%BROWSER% --include %TAG%ANDQA --argumentfile environments/common-arguments.txt --outputdir robot-results .
